fbc4d8deb97ba58715b063a8887224f456b7c160
test001 <oleksii.rodionov@itomy.ch>
2022-10-28 13:16:47 +0300

    Added task1 of lecture3

---
+++ lecture3/task1.mjs
@@ -0,0 +1,23 @@
+import readline, { Interface } from "readline"
+const program = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+ })
+
+console.log("To exit press 'Ctrl + C'")
+program.setPrompt("Please type 2-digits: (10-99) ")
+program.prompt()
+program.on("line", (input) => {
+   let isTwoDigits = /^[1-9][0-9]$/.test(input.trim())
+   switch (isTwoDigits) {
+      case true:
+         console.log(`Your digits are: ${input[0] + " and " + input[1]} \n`)
+         break;
+      default:
+         console.log("Invalid input!\n")
+         break;
+   }
+   program.prompt()
+}).on('close', () => {
+   console.log('\nBye Bye');
+ });
